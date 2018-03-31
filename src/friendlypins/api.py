e536d91c1fafb46be379c6919110e59fafaf55d5
Kevin Phillips <thefriendlycoder@gmail.com>
2018-03-30 23:16:56 -0300

    fixed build

--- src/friendlypins/api.py
+++ src/friendlypins/api.py
@@ -3,10 +3,10 @@ from __future__ import print_function
 
 class API(object):  # pylint: disable=too-few-public-methods
     """High level abstraction for the core Pinterest API"""
-    def __init__(self):  # pragma: no cover
+    def __init__(self):
         self.name = "hello"
 
-    def get_user(self, username=None):  # pragma: no cover
+    def get_user(self, username=None):
         """Gets all primitives associated with a particular Pinterst user
 
         :param str username:
--- unit_tests/test_api.py
+++ unit_tests/test_api.py
@@ -4,5 +4,9 @@ from friendlypins.api import API
 def test_constructor():
     obj = API()
 
+def test_get_user():
+    obj = API()
+    obj.get_user()
+
 if __name__ == "__main__":
     pytest.main([__file__, "-v", "-s"])
