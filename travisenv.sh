3b32851f64399281a6fe85f40f716888a2057ea3
Kevin Phillips <thefriendlycoder@gmail.com>
2018-03-30 22:15:16 -0300

    more build fixes

--- travisenv.sh
+++ travisenv.sh
@@ -2,5 +2,5 @@
 echo "Travis Python version: $TRAVIS_PYTHON_VERSION"
 [ $TRAVIS_PYTHON_VERSION == "pypy" ] && export TOXENV=pypy
 [ $TRAVIS_PYTHON_VERSION == "pypy3" ] && export TOXENV=pypy3
-[ -z "$TOKENENV" ] && export TOXENV=py`echo $TRAVIS_PYTHON_VERSION | tr "." "\n" | head -n 1`
-echo "TOX Python version: $TOKENENV"
+[ -z "$TOXENV" ] && export TOXENV=py`echo $TRAVIS_PYTHON_VERSION | tr "." "\n" | head -n 1`
+echo "TOX Python version: $TOXENV"
