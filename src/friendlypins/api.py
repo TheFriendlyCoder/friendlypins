4aade2d1f664c4654b217c133522e183f4158b7c
Kevin Phillips <thefriendlycoder@gmail.com>
2018-04-03 23:36:12 -0300

    Fixed #1 imiplemented initial user query

--- src/friendlypins/api.py
+++ src/friendlypins/api.py
@@ -1,10 +1,25 @@
 """Primary entry point for the Friendly Pinterest library"""
 from __future__ import print_function
+import logging
+import requests
+from friendlypins.user import User
 
 class API(object):  # pylint: disable=too-few-public-methods
     """High level abstraction for the core Pinterest API"""
-    def __init__(self):
-        self.name = "hello"
+
+    # URL of the root namespace for the Pinterest API
+    _root_url = 'https://api.pinterest.com/v1'
+
+    def __init__(self, personal_access_token):
+        """Constructor
+
+        :param str personal_access_token:
+            API authentication token used for secure access to a users'
+            Pinterest data
+        """
+
+        self._log = logging.getLogger(__name__)
+        self._token = personal_access_token
 
     def get_user(self, username=None):
         """Gets all primitives associated with a particular Pinterst user
@@ -15,11 +30,18 @@ class API(object):  # pylint: disable=too-few-public-methods
         returns: Pinterest user with the given name
         rtype: :class:`friendly_pinterest.user.User`
         """
-        print(self.name)
         if username:
-            return None
-        return None
+            raise NotImplementedError(
+                "Querying arbitrary Pinerest users is not yet supported.")
+        else:
+            temp_url = "{0}/me".format(self._root_url)
+        temp_url += "?access_token={0}".format(self._token)
+        response = requests.get(temp_url)
+        response.raise_for_status()
+        assert 'data' in response.json()
+        return User(response.json()['data'])
 
 
+# pylint: disable-all
 if __name__ == "__main__":
     pass
--- unit_tests/test_api.py
+++ unit_tests/test_api.py
@@ -1,12 +1,32 @@
 import pytest
+import mock
 from friendlypins.api import API
 
-def test_constructor():
-    obj = API()
-
 def test_get_user():
-    obj = API()
-    obj.get_user()
+    obj = API('abcd1234')
+    expected_url = 'https://www.pinterest.com/MyUserName/'
+    expected_firstname = "John"
+    expected_lastname = "Doe"
+    expected_id = "12345678"
+    expected_data = {
+        'data': {
+            'url': expected_url,
+            'first_name': expected_firstname,
+            'last_name': expected_lastname,
+            'id': expected_id
+        }
+    }
+    with mock.patch("friendlypins.api.requests") as mock_requests:
+        mock_response = mock.MagicMock()
+        mock_response.json.return_value = expected_data
+        mock_requests.get.return_value = mock_response
+        result = obj.get_user()
+
+        assert expected_url == result.url
+        assert expected_firstname == result.first_name
+        assert expected_lastname == result.last_name
+        assert expected_id == result.unique_id
+
 
 if __name__ == "__main__":
     pytest.main([__file__, "-v", "-s"])
